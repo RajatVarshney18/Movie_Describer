7e8b1c1a2966a61f4d93fef68adc42d28c49f3f6
RajatVarshney18 <rajatyo1862000@gmail.com>
2021-07-18 15:24:59 +0530

    Added Header and home component

--- movie-describer/src/app/app.module.ts
+++ movie-describer/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    MovieDetailsComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
